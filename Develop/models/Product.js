420d02eae166f7ece014f74606ab4e281666f05a
Asiak <asiakemp24@gmail.com>
2023-07-15 12:45:30 -0400

    created the product model

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, STRING } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,6 +10,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validates that the value is a decimal
+      validate: {
+        isDecimal: true,
+      },
+    }, 
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      // set default value of 10
+      defaultValue: 10,
+      // validates that the value is numeric
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
